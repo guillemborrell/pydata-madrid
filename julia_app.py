676f7d8f76015025222ff667070293f59d7510b0
Guillem Borrell <guillemborrell@gmail.com>
2016-03-21 19:17:56 +0100

    High performance parallel multithreaded web application with a key-value store.

--- julia_app.py
+++ julia_app.py
@@ -1,22 +1,28 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, redirect
 from julia_set import julia_set
 from PIL import Image
 from matplotlib import cm
 from itertools import product
+from threading import Thread
+from uuid import uuid4
 import numpy as np
 import time
 import io
 import julia_fast
 
+
 app = Flask(__name__)
 
-@app.route('/')
-def root():
+
+store = {}
+
+
+def parse_request():
     w = request.args.get('w')
-    if not w: w = 400
+    if not w: w = 600
     else: w = int(w)
     h = request.args.get('h')
-    if not h: h = 300
+    if not h: h = 400
     else: h = int(h)
     cre = request.args.get('cre')
     if not cre: cre = -0.8
@@ -27,9 +33,13 @@ def root():
     cmap = request.args.get('cmap')
     if not cmap: cmap = 'inferno'
 
+    return w, h, cre, cim, cmap
+
+
+def gen_image_stream(key, w, h, cre, cim, cmap):
     start = time.perf_counter()
     m = julia_fast.julia_set(w, h, cre + cim*1j)
-    print(time.perf_counter()-start)
+    end = time.perf_counter()-start
     
     image_data = np.empty((h,w,3), dtype=np.uint8)
     colors = 255*np.array(getattr(cm, cmap).colors)
@@ -42,13 +52,37 @@ def root():
     stream = io.BytesIO()
     image.save(stream, format='png')
     stream.seek(io.SEEK_SET)
-    
-    resp =  make_response(stream.read())
-    resp.headers['Content-Type'] = 'image/png'
+    print('Made the image in {} seconds'.format(end))
+    store[key] = stream.read()
 
+
+@app.route('/')
+def root():
+    key = str(uuid4())
+    img_thread = Thread(target=gen_image_stream, args=(key,*parse_request()))
+    img_thread.daemon = True
+    img_thread.start()
+
+    return '<http><body><a href="/image/{}">here</a></body></http>'.format(key)
+
+
+@app.route('/image/<key>')
+def image(key):
+    has_response = False
+    for i in range(120):
+        image = store.get(key)
+        if image:
+            resp = make_response(image)
+            resp.headers['Content-Type'] = 'image/png'
+            has_response = True
+
+        else:
+            resp = 'Not ready yet'
+            time.sleep(1)
+
+    if not has_response: resp = 'Something wrong happened'
     return resp
-
-
+    
 application = app
 
 
